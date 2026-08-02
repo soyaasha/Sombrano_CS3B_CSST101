70636001e46926481b06aa27620607f6c05aba45
Mrincess Allisha <mrincesssoyaasha831@gmail.com>
2026-08-02 23:18:33 +0800

    Add files via upload

---
+++ LAB_ACT3_CSST101.py
@@ -0,0 +1,13 @@
+print("\n")
+name = "Mrincess Allisha C. Sombrano"
+course = "Bachelor of Science in Computer Science"
+section = "BSCS - 3B"
+favorite_ai_tech = "Machine Learning"
+future_ai_prjct = "AI Disease Prediction System"
+
+print("Name: " + name)
+print("Course: " + course)  
+print("Section: " + section)
+print("Favorite AI Technology: " + favorite_ai_tech)
+print("Future AI Project: " + future_ai_prjct)
+print("\n")
